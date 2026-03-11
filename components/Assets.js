1b50dc1b3674cab972b0d13ee143113a9f15c1fb
M Aun Rasheed <greataun786@gmail.com>
2026-03-11 15:38:21 +0500

    Added 5th Project BuddyChat

---
+++ components/Assets.js
@@ -0,0 +1,43 @@
+export const projects = [
+    {
+        title: "Coffee Shop",
+        description: "A website to sell your cup of coffee online ☕",
+        stack: ["Next.js", "Tailwind"],
+        previewLink: "https://coffeeshop-7ad9.vercel.app/",
+        sourceLink: "https://github.com/MuhammadAunR/Coffee-Shop",
+        image: "/p1-coffeeshop.webp",
+    },
+    {
+        title: "Shop.CO",
+        description: "An online platform to sell your outfit ideas in this digital world 🛍️",
+        stack: ["Next.js", "Tailwind", "Strapi", "Node.js"],
+        previewLink: "",
+        sourceLink: "https://github.com/MuhammadAunR/E-Commerce-Store",
+        image: "/p2-e-store.webp",
+    },
+    {
+        title: "Tripzo",
+        description: "Tripzo — A modern travel platform to discover destinations, plan trips, and book experiences seamlessly 🚞",
+        stack: ["React.js", "Tailwind"],
+        previewLink: "https://tripzo-byma.vercel.app/",
+        sourceLink: "https://github.com/MuhammadAunR/Tripzo",
+        image: "/p3-tripzo.webp",
+    },
+    {
+        title: "Flavor Fiesta",
+        description: "A digital platform to discover and share delicious recipes from around the world, all in one place 🧑🏻‍🍳",
+        stack: ["React.js", "Tailwind", "JS-AOS"],
+        previewLink: "https://flavor-fiesta-byma.vercel.app/",
+        sourceLink: "https://github.com/MuhammadAunR/Flavor-Fiesta",
+        image: "/p4-cooking.webp",
+    },
+    {
+        title: "Buddy Chat",
+        description: "A simple and friendly AI chatbot built with React and Gemini API. Chat with your AI buddy anytime! 🤖",
+        stack: ["React.js", "Tailwind", "Context API", "GROQ API"],
+        previewLink: "https://buddy-chat-byma.vercel.app/",
+        sourceLink: "https://github.com/MuhammadAunR/Buddy-Chat",
+        image: "/p5-buddyChat.webp",
+    },
+
+];
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,46 +3,9 @@ import { ExternalLink } from 'lucide-react'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import Popup from './Popup'
-
-
+import { projects } from './Assets'
 
 const ProjectCard = () => {
-
-  const projects = [
-    {
-      title: "Coffee Shop",
-      description: "A website to sell your cup of coffee online ☕",
-      stack: ["Next.js", "Tailwind"],
-      previewLink: "https://coffeeshop-7ad9.vercel.app/",
-      sourceLink: "https://github.com/MuhammadAunR/Coffee-Shop",
-      image: "/p1-coffeeshop.webp",
-    },
-    {
-      title: "Shop.CO",
-      description: "An online platform to sell your outfit ideas in this digital world 🛍️",
-      stack: ["Next.js", "Tailwind", "Strapi", "Node.js"],
-      previewLink: "",
-      sourceLink: "https://github.com/MuhammadAunR/E-Commerce-Store",
-      image: "/p2-e-store.webp",
-    },
-    {
-      title: "Tripzo",
-      description: "Tripzo — A modern travel platform to discover destinations, plan trips, and book experiences seamlessly 🚞",
-      stack: ["React.js", "Tailwind"],
-      previewLink: "https://tripzo-byma.vercel.app/",
-      sourceLink: "https://github.com/MuhammadAunR/Tripzo",
-      image: "/p3-tripzo.webp",
-    },
-    {
-      title: "Flavor Fiesta",
-      description: "A digital platform to discover and share delicious recipes from around the world, all in one place 🧑🏻‍🍳",
-      stack: ["React.js", "Tailwind", "JS-AOS"],
-      previewLink: "https://flavor-fiesta-byma.vercel.app/",
-      sourceLink: "https://github.com/MuhammadAunR/Flavor-Fiesta",
-      image: "/p4-cooking.webp",
-    },
-
-  ];
 
   const [showPopup, setShowPopup] = useState(false)
   const [popupType, setPopupType] = useState(null)
