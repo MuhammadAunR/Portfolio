be1b26c42bbc4f9676184e267271c3381c8db5df
M Aun Rasheed <greataun786@gmail.com>
2026-06-22 03:15:59 -0700

    style: redesign the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,11 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <>
-            <nav className='flex justify-between items-center max-md:justify-center w-full 2xl:max-w-384 px-12 py-5 bg-white shadow-xl mb-14 fixed top-0 z-10' data-aos="fade-down">
-                <div className=''>
+  return (
+    <>
+      <nav className='flex justify-between items-center max-md:justify-center w-full 2xl:max-w-384 px-12 mb-14 fixed top-5 z-10' data-aos="fade-down">
+                <div className='bg-white/20 shadow-[2px_2px_10px_rgba(0,0,0,0.8)] backdrop-blur-lg rounded-full px-10 py-2.5'>
                     <h1 className="text-2xl font-semibold shine-wrapper">
                         <span className="base-text">
                             Muhammad Aun
@@ -15,7 +15,7 @@ const Navbar = () => {
                         </span>
                     </h1>
                 </div>
-                <ul className='flex gap-15 max-lg:gap-10 font-semibold text-xl max-md:hidden'>
+                <ul className='flex gap-10 max-lg:gap-10 font-semibold text-lg bg-white/50 shadow-[2px_2px_10px_rgba(0,0,0,0.8)] backdrop-blur-lg rounded-full px-7 py-3 max-md:hidden'>
                     <Link href={'#home'}>
                         <li className='cursor-pointer hover:text-main-color transition-colors ease-in-out'>Home</li>
                     </Link>
@@ -30,8 +30,8 @@ const Navbar = () => {
                     </Link>
                 </ul>
             </nav>
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
